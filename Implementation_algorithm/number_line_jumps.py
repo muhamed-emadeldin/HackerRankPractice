31e42d95a02b976e6c663db2b4ebd5c9ce7b9fe8
Muhamed <muhamed.emadeldin@outlook>
2020-12-23 08:29:02 +0300

    Number Line Jumps

--- Implementation_algorithm/number_line_jumps.py
+++ Implementation_algorithm/number_line_jumps.py
@@ -7,29 +7,41 @@ def kangaroo(x1, v1, x2, v2):
   assert(isinstance(v2, int)), "x1 should be int"
 
   #-->basic variables
-  k1 = x1 + v1
-  k2 = x2 + v2
+  k1      = x1 + v1
+  k2      = x2 + v2
+  bigger  = which_bigger(k1, k2)
 
   #-->basic condation
   if x1 < x2 and v1 <= v2:
     return "NO"
+  elif x1 > x2 and v1 >= v2:
+    return "NO"
 
-  while True:
+  while k1 != k2:
+    if abs(k1 - k2) > bigger:
+      return "NO", k1, k2
+    
     k1 += v1
     k2 += v2
-    
-    if k1 == k2:
-      return "YES"
 
-    if k1 > k2:
-      break
-  
-  return "NO"
+  return "YES", k1, k2
+
+def which_bigger(n1, n2):
+  if n1 > n2:
+    return n1
+  return n2
 
 # print(kangaroo(0,3, 4,2))
 # print(kangaroo(3,42, 2,94))
 # print(kangaroo(4,14, 2,98))
 # print(kangaroo(7,45, 2,56))
-print(kangaroo(2564,5393, 5121,2836))
+# print(kangaroo(2564,5393, 5121,2836))
+print(kangaroo(0,2, 5,3))
+print(kangaroo(2,1, 1,2))
+print(kangaroo(43, 2, 70, 2))
+
+
+
+
 
 
